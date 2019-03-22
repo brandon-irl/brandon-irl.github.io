3297eb9489efd3a2581d1e24f336b87ad07e3924
brandon-irl <4008993+brandon-irl@users.noreply.github.com>
2019-03-22 11:50:12 -0400

    update

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+String.format = function () {
+	var s = arguments[0];
+	for (var i = 0; i < arguments.length - 1; i++) {
+		var reg = new RegExp("\\{" + i + "\\}", "gm");
+		s = s.replace(reg, arguments[i + 1]);
+	}
+
+	return s;
+};
+
+(function ($) {
+	"use strict";
+
+	var skills = {
+		'gitlab': { name: 'GitLab', presentationType: 'image' },
+		'jira': { name: 'Jira', presentationType: 'image' },
+		'.net': { name: '.NET', presentationType: 'pill' },
+		'angular': { name: 'Angular', presentationType: 'image' },
+		'aws': { name: 'AWS', presentationType: 'image' },
+		'kanban': { name: 'Kanban', presentationType: 'pill' },
+		'windows': { name: 'Windows', presentationType: 'image' },
+		'linux': { name: 'Linux', presentationType: 'image' },
+		'apple': { name: 'Apple', presentationType: 'image' },
+	};
+
+	var getBadgeForSkill = function (skill) {
+		if (skill == null)
+			return null;
+
+		switch (skill.presentationType) {
+			case 'image':
+				return $('<i/>').addClass(String.format('fab fa-lg fa-{0} {0}', skill.name.toLowerCase()));
+			case 'pill':
+			default:
+				return $('<span></span>').addClass('badge badge-pill ' + skill.name.toLowerCase()).html(skill.name);
+		}
+	};
+
+	$('#experience div .ritem .resume-content ul').each(function () {
+		var that = $(this);
+		var s = that.attr('skills');
+		if (s) {
+			$(s.split(' ')).each(function () {
+				var li = $('<li />')
+					.addClass('list-inline-item')
+					.appendTo(that);
+				getBadgeForSkill(skills[this] || { name: this, presentationType: 'pill' }).appendTo(li);
+			})
+		};
+	});
+
+})(jQuery);
